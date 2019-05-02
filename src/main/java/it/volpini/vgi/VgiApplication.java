0eec1faa9ba3c0c09fc3f912c8ee0c853e396d40
Taba90 <tabareu90@gmail.com>
2019-05-02 16:15:39 +0200

    war deployment stuff

--- src/main/java/it/volpini/vgi/VgiApplication.java
+++ src/main/java/it/volpini/vgi/VgiApplication.java
@@ -2,9 +2,16 @@ package it.volpini.vgi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VgiApplication {
+public class VgiApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(VgiApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(VgiApplication.class, args);
